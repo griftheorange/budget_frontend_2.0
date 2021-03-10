a9d02f5046987912aad034e316a75df3f13bcfb8
Griffin Poole <griffinpoole5@gmail.com>
2021-03-10 14:30:59 -0500

    Fixed error in index import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import defaultStore from './Redux/defaultStore';
+import defaultStore from './redux/defaultStore';
 
 import AppRouter from './AppRouter';
 
